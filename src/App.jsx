5e0df4ac17dbefbd45191ec7eb12b0c8e22db33e
YuuY314 <rafaelyu0202@gmail.com>
2026-01-09 14:20:55 -0300

    Lesson 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import './App.css'
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      <h1>Olá Mundo!</h1>
+      <Header/>
+      <section className="container">
+        <div className="presentation">
+          <p>
+            Olá, sou <br/>
+            <span>Rafael Yu</span> <br/>
+            Dev Full Stack
+          </p>
+          <button className="btn btn-red">
+            Saiba mais sobre mim
+          </button>
+        </div>
+        <figure>
+          <img className="img-home" src="/developer-red.svg" alt="Imagem de Home"/>
+        </figure>
+      </section>
+      <Footer/>
     </>
   )
 }
---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./Footer.module.css";
+
+function Footer() {
+    return (
+        <footer className={styles.footer}>
+            Feito com ❣️ por Rafael Yu &copy; 2026
+        </footer>
+    );
+}
+
+export default Footer;
