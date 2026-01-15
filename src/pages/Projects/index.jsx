4b0cf363516e4acae3249a8cbfb256784d65e28a
YuuY314 <rafaelyu0202@gmail.com>
2026-01-15 13:44:57 -0300

    Lesson 8

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,9 +1,13 @@
+import Card from "../../components/Card";
 import styles from "./Projects.module.css";
 
 function Projects() {
     return (
         <>
             <h2>Projetos</h2>
+            <Card/>
+            <Card/>
+            <Card/>
         </>
     );
 }
