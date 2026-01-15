160d2909be46a5b9e5205de833b82bdc470a10bc
YuuY314 <rafaelyu0202@gmail.com>
2026-01-15 17:28:18 -0300

    Lesson 12

--- src/pages/Page404/index.jsx
+++ src/pages/Page404/index.jsx
@@ -2,13 +2,13 @@ import styles from "./Page404.module.css";
 
 function Page404() {
     return (
-        <>
+        <section className={styles.page404}>
             <h2 className={styles.title2}>Algo de errado não está certo</h2>
             <div className={styles.texts}>
                 <span className={styles.big_text}>404</span>
                 <strong className={styles.red_text}>Página não localizada!</strong>
             </div>
-        </>
+        </section>
     );
 }
 
