cd7543060374f3da5f527434b2a8f16d463a7910
YuuY314 <rafaelyu0202@gmail.com>
2026-01-15 14:14:26 -0300

    Lesson 9

--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -1,14 +1,41 @@
+import { useEffect, useState } from "react";
 import Card from "../../components/Card";
 import styles from "./Projects.module.css";
 
 function Projects() {
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        const searchRepositories = async () => {
+            const response = await fetch("https://api.github.com/users/yuuy314/repos");
+            const data = await response.json();
+            setRepositories(data);
+        }
+        searchRepositories();
+    }, []);
+
     return (
-        <>
+        <section className={styles.projects}>
             <h2>Projetos</h2>
-            <Card/>
-            <Card/>
-            <Card/>
-        </>
+            {
+                repositories.length > 0 ? (
+                    <section className={styles.list}>
+                        {
+                            repositories.map((repo) => (
+                                <Card
+                                    key={repo.id}
+                                    name={repo.name}
+                                    description={repo.description}
+                                    html_url={repo.html_url}
+                                />
+                            ))
+                        }
+                    </section>
+                ) : (
+                    <p>Carregando repositórios...</p>
+                )
+            }
+        </section>
     );
 }
 
